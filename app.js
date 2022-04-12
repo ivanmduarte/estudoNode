0ba6bcf39a52f80bdf991da249393eecaa008fc3
ivanmduarte <ivan.mduarte@live.com>
2022-04-11 21:51:17 -0300

    Implementação de rotas GET

--- app.js
+++ app.js
@@ -32,7 +32,17 @@ app.post("/products", (request, response) => {
 
     products.push(product);
 
+    return response.json(product);
+});
+
+app.get("/products", (request, response) => {
     return response.json(products);
+});
+
+app.get("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const product = products.find(product => product.id === id);
+    return response.json(product);
 });
 
 app.listen(4002, ()=> console.log("Servidor ativo na porta 4002"));
