92cbe5a0e9009f920a2aebdd16f5ef6d76005909
ivanmduarte <ivan.mduarte@live.com>
2022-04-11 22:32:50 -0300

    Primeiro DELETE

--- app.js
+++ app.js
@@ -60,4 +60,13 @@ app.put("/products/:id", (request, response) => {
 
 });
 
+app.delete("/products/:id", (request, response) =>{
+    const { id } = request.params;
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex, 1);
+
+    return response.json({ message: "Produto removido com sucesso"});
+});
+
 app.listen(4002, ()=> console.log("Servidor ativo na porta 4002"));
