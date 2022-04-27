1bc06238e3eda163c6ed47b283b14d3040a9549e
ivanmduarte <ivan.mduarte@live.com>
2022-04-27 18:57:33 -0300

    Products in File
    gravando os valores de products em um arquivo json.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const { response } = require("express");
-const express = require("express");
-const { randomUUID } = require("crypto");
+const { response }      = require("express");
+const express           = require("express");
+const { randomUUID }    = require("crypto");
+const fs                = require('fs');
 
 const app = express();
 
@@ -12,7 +13,18 @@ const app = express();
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        products = JSON.parse(data);
+    }
+});
 
 /**
  * Body => Sempre que eu quiser enviar dados para minha aplicação
@@ -21,9 +33,7 @@ const products = [];
  */
 
 app.post("/products", (request, response) => {
-    
     const { name, price } = request.body;
-
     const product = {
         name,
         price,
@@ -31,6 +41,8 @@ app.post("/products", (request, response) => {
     }
 
     products.push(product);
+
+    productFile();
 
     return response.json(product);
 });
@@ -40,33 +52,55 @@ app.get("/products", (request, response) => {
 });
 
 app.get("/products/:id", (request, response) => {
-    const { id } = request.params;
-    const product = products.find(product => product.id === id);
+    const { id }    = request.params;
+    const product   = products.find(product => product.id === id);
+
     return response.json(product);
 });
 
 app.put("/products/:id", (request, response) => {
-    const { id } = request.params;
-    const { name, price } = request.body;
-
-    const productIndex = products.findIndex(product => product.id === id);
+    const { id }            = request.params;
+    const { name, price }   = request.body;
+    const productIndex      = products.findIndex(product => product.id === id);
+    
     products[productIndex] = {
         ...products[productIndex],
         name,
         price
     }
 
-    return response.json({ message: "Produto alterado com sucesso" });
+    productFile();
 
+    return response.json({ message: "Produto alterado com sucesso" });
 });
 
-app.delete("/products/:id", (request, response) =>{
-    const { id } = request.params;
-    const productIndex = products.findIndex(product => product.id === id);
+app.delete("/products/:id", (request, response) => {
+    const { id }        = request.params;
+    const productIndex  = products.findIndex(product => product.id === id);
 
     products.splice(productIndex, 1);
+
+    productFile();
 
     return response.json({ message: "Produto removido com sucesso"});
 });
 
-app.listen(4002, ()=> console.log("Servidor ativo na porta 4002"));
+/**
+ * Função responsável por gravar o products no arquivo products.json
+ */
+function productFile()
+{
+    fs.writeFile('products.json', JSON.stringify(products), (err) => {
+        
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Produto inserido");
+        }
+    });
+}
+
+app.listen(4002, () => console.log("Servidor ativo na porta 4002"));
